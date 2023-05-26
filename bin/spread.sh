defeccd0065b9b811ed0ebd455c7f722aea6ce4f
kkirino <kosuke.kirino@gmail.com>
2023-05-26 20:48:39 +0900

    [fix] bin/spread.sh の軽微なミスを修正

--- bin/spread.sh
+++ bin/spread.sh
@@ -4,7 +4,7 @@ DIR_DOTFILES_BIN=$(cd $(dirname $0); pwd)
 DIR_DOTFILES_ROOT=$(dirname ${DIR_DOTFILES_BIN})
 
 sudo cp ${DIR_DOTFILES_ROOT}/wsl.conf /etc/
-cp {DIR_DOTFILES_ROOT}/.bashrc ${HOME}
-cp {DIR_DOTFILES_ROOT}/.vimrc ${HOME}
-cp {DIR_DOTFILES_ROOT}/coc-settings.json ${HOME}/.vim
+cp ${DIR_DOTFILES_ROOT}/.bashrc ${HOME}
+cp ${DIR_DOTFILES_ROOT}/.vimrc ${HOME}
+cp ${DIR_DOTFILES_ROOT}/coc-settings.json ${HOME}/.vim
 
