87bf81fd605aeb3a611b6a10342cf9799570fe37
kkirino <kosuke.kirino@gmail.com>
2023-07-03 17:11:33 +0900

    Windows PowerShell, PowerShell の両者に対応

--- spread.sh
+++ spread.sh
@@ -3,20 +3,27 @@
 ROOT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
 
-# set dotfiles in WSL2
+# WSL2 の各種 dotfiles を配置する
 sudo cp "${ROOT_DIR}"/wsl2/wsl.conf /etc/
 cp "${ROOT_DIR}"/wsl2/.bashrc "${HOME}"
 cp "${ROOT_DIR}"/wsl2/.vimrc "${HOME}"
 cp "${ROOT_DIR}"/wsl2/coc-settings.json "${HOME}"/.vim
 
 
-# set dotfiles in Windows
+# Windows の各種 dotfiles を配置する
 WIN_USER=kkirino
 WIN_HOME_DIR=/mnt/c/Users/"${WIN_USER}"
+# Neovim
 NVIM_DIR="${WIN_HOME_DIR}"/AppData/Local/nvim/
-PS_PROFILE_DIR="${WIN_HOME_DIR}"/Documents/WindowsPowerShell/
 [ -d "${NVIM_DIR}" ] || mkdir -p "${NVIM_DIR}"
 cp "${ROOT_DIR}"/windows/init.vim "${NVIM_DIR}"
+# VSCode
 cp "${ROOT_DIR}"/windows/settings.json "${WIN_HOME_DIR}"/AppData/Roaming/Code/User
+# Windows PowerShell (不要であれば以下の 3 行をコメントアウト)
+WPS_PROFILE_DIR="${WIN_HOME_DIR}"/Documents/WindowsPowerShell/
+[ -d "${PS_PROFILE_DIR}" ] || mkdir -p "${WPS_PROFILE_DIR}"
+cp "${ROOT_DIR}"/windows/Microsoft.PowerShell_profile.ps1 "${WPS_PROFILE_DIR}"
+# PowerShell (不要であれば以下の 3 行をコメントアウト)
+PS_PROFILE_DIR="${WIN_HOME_DIR}"/Documents/pwershell/
 [ -d "${PS_PROFILE_DIR}" ] || mkdir -p "${PS_PROFILE_DIR}"
 cp "${ROOT_DIR}"/windows/Microsoft.PowerShell_profile.ps1 "${PS_PROFILE_DIR}"
